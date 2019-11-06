9bb9af7228ec5f4d9a89faa8a5f7711d952fe1c0
cloudinfra <cloudinfra@FLH9Aai085.tky.mesh.ad.jp>
2019-11-06 07:37:00 -0500

    could init network

--- mkimage/mkimg.sh
+++ mkimage/mkimg.sh
@@ -4,6 +4,6 @@ if [ ! -f CentOS-7-x86_64-Minimal-1908.iso ]; then
   wget http://ftp-srv2.kddilabs.jp/Linux/packages/CentOS/7.7.1908/isos/x86_64/CentOS-7-x86_64-Minimal-1908.iso
 fi
 
-virt-install   --name mkimg   --hvm   --virt-type kvm   --ram 1024   --vcpus 1   --arch x86_64   --os-type linux   --os-variant rhel7   --boot hd   --disk mkimg.qcow2,size=5,format=qcow2   --graphics none   --serial pty   --console pty   --location CentOS-7-x86_64-Minimal-1908.iso   --initrd-inject mkimg.ks.cfg   --extra-args "inst.ks=file:/mkimg.ks.cfg console=ttyS0"
+virt-install   --name mkimg   --hvm   --virt-type kvm   --ram 1024   --vcpus 1   --arch x86_64   --os-type linux   --os-variant rhel7   --boot hd   --disk mkimg.qcow2,size=5,format=qcow2   --graphics none   --serial pty   --console pty   --location CentOS-7-x86_64-Minimal-1908.iso   --noreboot --initrd-inject mkimg.ks.cfg   --extra-args "inst.ks=file:/mkimg.ks.cfg console=ttyS0"
 
 #virsh undefine mkimg
--- mkimage/mkmeta.sh
+++ mkimage/mkmeta.sh
@@ -10,4 +10,10 @@ tmpfile=$(mktemp)
 dd if=/dev/zero of=$tmpfile bs=1M count=5
 mkfs.ext3 -F $tmpfile
 mount -o loop -t ext3 $tmpfile $tmpdir
-
+cat <<EOF > $tmpdir/cloudinit.sh
+#!/bin/sh
+nmcli c mod eth0 connection.autoconnect yes ipv4.method manual ipv4.addresses 192.168.1.1/24 ipv4.gateway 192.168.1.254
+EOF
+umount $tmpdir
+rmdir $tmpdir
+echo $tmpfile
