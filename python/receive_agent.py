e7d35f3e98a7c6437b1232b4700c28b1db03f04a
takayuki.yasunaga <yasu@takayukiyasunaganoMacBook-ea.local>
2019-11-06 20:55:09 +0900

    キュー投げる部分とエージェント部分を修正

--- python/receive_agent.py
+++ python/receive_agent.py
@@ -3,23 +3,17 @@ import libvirt
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='pleaseCreate')
+channel.queue_declare(queue='pleaseStart')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    #あとでここをVM立ち上げに置き換える
-    connect = libvirt.open("qemu:///system")
-    for id in conn.listDomainsID():
-        dom = conn.lookupByID(id)
-        infos = dom.info()
-        print 'ID = %d' % id
-        print 'Name =  %s' % dom.name()
-        print 'State = %d' % infos[0]
-        print 'Max Memory = %d' % infos[1]
-        print 'Number of virt CPUs = %d' % infos[3]
-        print 'CPU Time (in ns) = %d' % infos[2]
-        print ' '
+    print(' [x] Received %r' % body)
+    connect = libvirt.open('qemu:///system')
+    for id in connect.listDomainsID():
+        vm = connect.lookupByID(id)
+        print('starting'+vm.name())
+        vm.start()
+        tim.sleep(1)
 
-channel.basic_consume(callback, queue='pleaseCreate', no_ack=True)
+channel.basic_consume(callback, queue='pleaseSreate', no_ack=True)
 
 channel.start_consuming()
--- python/send.py
+++ python/send.py
@@ -4,7 +4,28 @@ credentials = pika.PlainCredentials('cloudinfra', 'cloudinfra01')
 parameters = pika.ConnectionParameters('10.0.0.2', 5672,'/',credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
-channel.queue_declare(queue='pleaseCreate')
-channel.basic_publish(exchange='', routing_key='pleaseCreate', body='please create VM')
-print(" [x] Sent 'please create VM'")
-connection.close()
+
+def order(word,instanceID):
+    if 'create' in word:
+        channel.queue_declare(queue='pleaseCreate')
+        channel.basic_publish(exchange='', routing_key='pleaseCreate', body='please create VM'+'_'+instanceID)
+        print(" [x] Sent 'please create VM'")
+        connection.close()
+
+    elif 'start' in word:
+        channel.queue_declare(queue='pleaseStart')
+        channel.basic_publish(exchange='', routing_key='pleaseStart', body='please start VM'+'_'+instanceID)
+        print(" [x] Sent 'please start VM'")
+        connection.close()
+
+    elif 'stop' in word:
+        channel.queue_declare(queue='pleaseStop')
+        channel.basic_publish(exchange='', routing_key='pleaseStop', body='please stop VM'+'_'+instanceID)
+        print(" [x] Sent 'please stop VM'")
+        connection.close()
+
+    elif 'destroy' in word:
+        channel.queue_declare(queue='pleaseDestroy')
+        channel.basic_publish(exchange='', routing_key='pleaseDestroy', body='please destroy VM'+'_'+instanceID)
+        print(" [x] Sent 'please destroy VM'")
+        connection.close()
