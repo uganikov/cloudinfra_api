81763030daa9b09db1557cdc223042176780b500
Shingo Akagaki <akagas@amazon.co.jp>
2019-11-12 21:26:23 +0900

    now can assign ip but network address is fixed to 192.168.1. for simplify the implementation

--- mkimage/mkmeta.sh
+++ mkimage/mkmeta.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ${EUID:-${UID}} = 0 ]; then
     echo 'this script needs root permission.'
 fi
+
+network_addr=192.168.1.
+host_addr=$1
+shift
+mask=24
+shift
+gw=254
 
 tmpdir=$(mktemp -d)
 tmpfile=$(mktemp)
@@ -12,7 +19,7 @@ mkfs.ext3 -F $tmpfile
 mount -o loop -t ext3 $tmpfile $tmpdir
 cat <<EOF > $tmpdir/cloudinit.sh
 #!/bin/sh
-nmcli c mod eth0 connection.autoconnect yes ipv4.method manual ipv4.addresses 192.168.1.1/24 ipv4.gateway 192.168.1.254
+nmcli c mod eth0 connection.autoconnect yes ipv4.method manual ipv4.addresses $network_addr$host_addr/$mask ipv4.gateway ${network_addr}254
 EOF
 umount $tmpdir
 rmdir $tmpdir
