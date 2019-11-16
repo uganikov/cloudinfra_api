3ba1d4e1daca246c56f603246c8f828abc1568f2
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-11-16 10:56:42 -0500

    publish vm status

--- agent/receive_agent.py
+++ agent/receive_agent.py
@@ -4,11 +4,25 @@ import json
 import subprocess
 import os
 import time
+import pprint
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='10.0.0.1'))
 channel = connection.channel()
 channel.queue_declare(queue='cloud_infra_api')
 channel.queue_declare(queue='cloud_infra_api_result')
+
+def publish_instance_status(vm):
+    print('show '+vm.name())
+    vminfo = vm.info()
+    result = {
+      "msg":"status",
+      "instance_id": vm.name(),
+      "state": vminfo[0],
+      "memory": vminfo[2],
+      "cpu": vminfo[3]
+    }
+    channel.basic_publish("", "cloud_infra_api_result", json.dumps(result))
 
 def callback(ch, method, properties, body):
     print(' [x] Received %r' % body)
@@ -21,7 +35,7 @@ def callback(ch, method, properties, body):
         print('  instance_id: ' + instance_id)
         print('           ip: ' + ip)
         fn = subprocess.check_output(["sudo", "bash", "./mkmeta.sh", ip, instance_id])
-        os.system("sed -e 's/@instance_id@/" + instance_id + "/g' template.xml > " + instance_id + ".xml")
+        os.system("sed -e 's/@instance_id@/" + instance_id + "/g' -e 's|@image_path@|" + os.getcwd() + "|g' template.xml > " + instance_id + ".xml")
 
         connect = libvirt.open('qemu:///system')
         xmlfile = open(instance_id + ".xml")
@@ -32,6 +46,7 @@ def callback(ch, method, properties, body):
         vm = connect.lookupByName(instance_id)
         print('starting'+vm.name())
         vm.create()
+        publish_instance_status(vm)
 
 channel.basic_consume(callback, queue='cloud_infra_api', no_ack=True)
 
@@ -48,8 +63,7 @@ def pubsub_callback(ch, method, properties, body):
         instance_id = params["instance_id"]
         connect = libvirt.open('qemu:///system')
         vm = connect.lookupByName(instance_id)
-        print('show'+vm.name())
-        channel.basic_publish("", "cloud_infra_api_result", json.dumps({"name": vm.name()}))
+        publish_instance_status(vm)
     elif cmd == "destroy":
         instance_id = params["instance_id"]
         connect = libvirt.open('qemu:///system')
