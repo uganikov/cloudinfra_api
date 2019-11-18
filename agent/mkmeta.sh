e1edccaf7d6cbf1f91070abd664da79777b0891a
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-11-18 20:05:44 +0900

    distribute public key remove cpu feature

--- agent/mkmeta.sh
+++ agent/mkmeta.sh
@@ -18,11 +18,16 @@ tmpfile=$(mktemp)
 dd if=/dev/zero of=$tmpfile bs=1M count=5
 mkfs.ext3 -F $tmpfile
 mount -o loop -t ext3 $tmpfile $tmpdir
-cp ${instance_id}.pub $tmpfile
+cp ${instance_id}.pub $tmpdir
 cat <<EOF > $tmpdir/cloudinit.sh
 #!/bin/sh
 nmcli c mod eth0 connection.autoconnect yes ipv4.method manual ipv4.addresses $network_addr$host_addr/$mask ipv4.gateway ${network_addr}254
+mkdir -p /home/cloudinfra/.ssh/
+chown cloudinfra:cloudinfra /home/cloudinfra/.ssh/
+chmod 700 /home/cloudinfra/.ssh/
 cp /var/metadrive/${instance_id}.pub /home/cloudinfra/.ssh/authorized_keys
+chown cloudinfra:cloudinfra /home/cloudinfra/.ssh/authorized_keys
+chmod 400 /home/cloudinfra/.ssh/authorized_keys
 EOF
 umount $tmpdir
 rmdir $tmpdir
