45a330c0d4c8e814decb3f5e5222bbe920ace3ef
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-11-19 10:44:22 -0500

    destroy

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -10,8 +10,12 @@ class InstancesController < ApplicationController
 
   # GET /instances/1
   def show
-    mq.publish({cmd: "show", instance_id: "i-#{@instance.public_uid}"})
-    render json: @instance
+    if @instance.nil?
+      render status: 404, json: {status: 404, instance_id: params[:id]}
+    else
+      mq.publish({cmd: "show", instance_id: "i-#{@instance.public_uid}"})
+      render json: @instance
+    end
   end
 
   # POST /instances
@@ -27,17 +31,27 @@ class InstancesController < ApplicationController
 
   # PATCH/PUT /instances/1
   def update
-    if @instance.update(instance_params)
-      render json: @instance
+    if @instance.nil?
+      render status: 404, json: {status: 404, instance_id: params[:id]}
     else
-      render json: @instance.errors, status: :unprocessable_entity
+      status = instance_params[:status].to_i
+      if status == 0
+        mq.publish({cmd: "stop", instance_id: "i-#{@instance.public_uid}"})
+      elsif status == 1
+        mq.publish({cmd: "start", instance_id: "i-#{@instance.public_uid}"})
+      end
+      render json: @instance
     end
   end
 
   # DELETE /instances/1
   def destroy
-    mq.publish('cloud_infra_api_pubsub', {cmd: "destroy", instance_id: "i-#{@instance.public_uid}"})
-#    @instance.destroy
+    if @instance.nil?
+      render status: 404, json: {status: 404, instance_id: params[:id]}
+    else
+      mq.publish({cmd: "destroy", instance_id: "i-#{@instance.public_uid}"})
+      render json: {cmd: "destroy", instance_id: "i-#{@instance.public_uid}", status: "accepted"}
+    end
   end
 
   private
@@ -52,5 +66,6 @@ class InstancesController < ApplicationController
     def instance_params
       params.require(:instance).permit(:public_uid)
       params.require(:instance).permit(:ip)
+      params.require(:instance).permit(:status)
     end
 end
